5da4da481b308f3f0c11d031fc80849cb0609377
Gevorg <amiraghyangevorg99@gmail.com>
2022-11-25 19:34:41 +0400

    test comment

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+git add .
+
+echo "Enter Commit Name:"
+read commit
+
+if [ -z $commit ]; then
+  echo "Empty commit try again:"
+  exit
+fi
+
+echo $commit
+
+
+git commit -m "$commit"
+
+git push origin main
