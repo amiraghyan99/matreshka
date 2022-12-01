8232a1abd5a9f4b04c5f6294d7de2fc8e821231a
Gevorg <amiraghyangevorg99@gmail.com>
2022-12-01 18:48:50 +0400

    divider

--- resources/js/frontend.js
+++ resources/js/frontend.js
@@ -1,25 +1,5 @@
 import '../css/frontend/main.css';
 
-import '../js/Frontend/js/bootstrap.bundle.min.js'
-import '../js/Frontend/js/countdown.js'
-import '../js/Frontend/js/google-map.js'
-import '../js/Frontend/js/html5shiv.js'
-import '../js/Frontend/js/meanmenu.js'
-import '../js/Frontend/js/html5shiv-printshiv.js'
-import '../js/Frontend/js/isotope.pkgd.min.js'
-import '../js/Frontend/js/isotope_custom.js'
-import '../js/Frontend/js/jquery-3.6.0.min.js'
-import '../js/Frontend/js/jquery-migrate-3.3.2.min.js'
-import '../js/Frontend/js/jquery.gridrotator.js'
-import '../js/Frontend/js/jquery.magnific-popup.min.js'
-import '../js/Frontend/js/jquery.waypoints.min.js'
-import '../js/Frontend/js/masonry.pkgd.min.js'
-import '../js/Frontend/js/modernizr-3.11.2.min.js'
-import '../js/Frontend/js/owl.carousel.min.js'
-import '../js/Frontend/js/popper.js'
-import '../js/Frontend/js/scrollreveal.min.js'
-import '../js/Frontend/js/theme.js'
-
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -9,6 +9,7 @@
     <!-- Scripts -->
     @routes
     @vite('resources/js/frontend.js')
+    @vite('resources/js/Frontend/js/main.js')
     @inertiaHead
 </head>
 <body data-spy="scroll" data-target="#scroll-menu" data-offset="65">
