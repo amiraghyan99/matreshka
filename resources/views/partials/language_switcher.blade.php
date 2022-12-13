395e55cf5516d83e719d1db3c3eeec083f76de3b
Gevorg <amiraghyangevorg99@gmail.com>
2022-12-13 15:27:22 +0400

    qwerty

--- resources/views/partials/language_switcher.blade.php
+++ resources/views/partials/language_switcher.blade.php
@@ -1,28 +1,18 @@
 @section('language_switcher')
-<div class="dropdown localization">
-    <div class="dropdown-toggle" id="Localization" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-        <img width="20px" src="{{url('assets/images/svg/flags', $available_locales[app()->currentLocale()].'.svg')}}"
-             alt="">
-    </div>
-    <ul class="dropdown-menu" aria-labelledby="Dropdown">
-        @foreach($available_locales as  $available_locale => $locale_name)
-            @if($available_locale === app()->getLocale())
+    <div class="dropdown localization">
+        <div class="dropdown-toggle" id="Localization" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+            <img width="20px" src="{{url('assets/img/svg/flags', app()->currentLocale().'.svg')}}"
+                 alt="">
+        </div>
+        <ul class="dropdown-menu" aria-labelledby="Dropdown">
+            @foreach($available_locales as  $available_locale => $locale_name)
                 <li>
                     <a class="dropdown-item" href="{{route('localization', $available_locale)}}">
-                        <img width="20px" src="{{url('assets/images/svg/flags',  strtolower($locale_name). '.svg' )}}"
+                        <img width="20px" src="{{url('assets/img/svg/flags' , $available_locale. '.svg')}}"
                              alt="{{$locale_name}}">
-                        <i class="fa fa-check text-success"></i>
                     </a>
                 </li>
-            @else
-                <li>
-                    <a class="dropdown-item" href="{{route('localization', $available_locale)}}">
-                        <img width="20px" src="{{url('assets/images/svg/flags' , strtolower($locale_name). '.svg')}}"
-                             alt="$locale_name">
-                    </a>
-                </li>
-            @endif
-        @endforeach
-    </ul>
-</div>
+            @endforeach
+        </ul>
+    </div>
 @endsection
